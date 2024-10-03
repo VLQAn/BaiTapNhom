909f6ddc2219ab2aa1a5386c0371c73da769ca9e
thtnv <thtnv9876@gmail.com>
2024-10-04 00:13:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bgk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bgk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
